df709ab69094d19dc4350cf17d30c877cf24b2e1
chanfuguo <chanfuguo@didichuxing.com>
2019-07-05 14:42:29 +0800

    add for master

--- example/src/App.js
+++ example/src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
+import snabbt from 'snoopy'
 // import * as Rematrix from 'rematrix';
 
-import Snoopy from 'snoopy';
-window.Snoopy = Snoopy;
+// import Snoopy, {Matrix} from 'snoopy';
+// window.Snoopy = Snoopy;
+// window.Matrix = Matrix;
 export default class App extends Component {
   componentDidMount() {
      
 
   }
   start() {
+
     const hasActive = this.refs.test1.classList.contains('active');
     let test = this.refs.test;
-    Snoopy(test, {
-      rotation: [0, Math.PI * 3, 0],
+    // const snoopy = window.snoopy = new Snoopy(test, {
+    //   rotation: [0, Math.PI * 3, 0],
+    //   scale:[.5,.5],
+    //   easing: 'ease',
+    //   duration: 3000
+    // })
+    
+    snabbt(test, {
+      fromPerspective:300,
+      perspective: 200,
+      scale:[.5,.5],
+      rotation:[0, Math.PI, 0],
+      // position:[300,0,0],
+      // rotationPost:[0, -Math.PI * 2, 0],
+
       easing: 'ease',
-      duration: 3000
+      duration: 4000
     })
    
     /**
@@ -23,9 +39,9 @@ export default class App extends Component {
     // 正确的 matrix3d(-0.939693, -0.34202, 0, 0, 0.34202, -0.939693, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
     // 我的   matrix3d(-0.9396926207859084,0.34202014332566866,0,0,-0.34202014332566866,-0.9396926207859084,0,0,0,0,1,0,0,0,0,1)
     
-    if (!hasActive) {
-      this.refs.test1.classList.add('active');
-    }
+    // if (!hasActive) {
+    //   this.refs.test1.classList.add('active');
+    // }
     
   }
   render () {
